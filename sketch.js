0d5857ce5115ec84b78cff35d258001d7f579352
elias <rshack@unca.edu>
2023-04-27 15:25:51 -0400

    SNAKEGAME4/27

--- sketch.js
+++ sketch.js
@@ -1,74 +1,61 @@
-//board
-var blockSize = 25;
-var rows = 20;
-var cols = 20;
-var board;
-var context; 
-
-//snake head
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
-
-var velocityX = 0;
-var velocityY = 0;
-
-var snakeBody = [];
-
-//food
-var foodX;
-var foodY;
-
-var gameOver = false;
+const blockSize = 25;
+const rows = 21;
+const cols = 21;
+let board;
+let context; 
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+let velocityX = 0;
+let velocityY = 0;
+let snakeBody = [];
+let foodX;
+let foodY;
+let gameOver = false;
 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
-    context = board.getContext("2d"); //used for drawing on the board
-
+    context = board.getContext("2d");
     placeFood();
     document.addEventListener("keyup", changeDirection);
-    // update();
-    setInterval(update, 1000/10); //100 milliseconds
+    setInterval(update, 1000/10);
+
+
+board.style.position = "absolute";
+    board.style.top = "50%";
+    board.style.left = "50%";
+    board.style.transform = "translate(-50%, -50%)";
 }
 
 function update() {
-    if (gameOver) {
-        return;
-    }
-
-    context.fillStyle="black";
+    if (gameOver) return;
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, board.width, board.height);
-
-    context.fillStyle="red";
+    context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
-
     if (snakeX == foodX && snakeY == foodY) {
         snakeBody.push([foodX, foodY]);
         placeFood();
     }
-
     for (let i = snakeBody.length-1; i > 0; i--) {
         snakeBody[i] = snakeBody[i-1];
     }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
-    }
-
-    context.fillStyle="lime";
+    if (snakeBody.length) snakeBody[0] = [snakeX, snakeY];
+    context.fillStyle = "lime";
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    context.beginPath();
+    context.arc(snakeX + blockSize/2, snakeY + blockSize/2, blockSize/2, 0, 2*Math.PI);
+    context.fillStyle = "darkgreen";
+    context.fill();
     for (let i = 0; i < snakeBody.length; i++) {
         context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
     }
-
-    //game over conditions
     if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
         gameOver = true;
         alert("Game Over");
     }
-
     for (let i = 0; i < snakeBody.length; i++) {
         if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
             gameOver = true;
@@ -81,24 +68,19 @@ function changeDirection(e) {
     if (e.code == "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    }
-    else if (e.code == "ArrowDown" && velocityY != -1) {
+    } else if (e.code == "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    }
-    else if (e.code == "ArrowLeft" && velocityX != 1) {
+    } else if (e.code == "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    }
-    else if (e.code == "ArrowRight" && velocityX != -1) {
+    } else if (e.code == "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
 }
 
-
 function placeFood() {
-    //(0-1) * cols -> (0-19.9999) -> (0-19) * 25
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * rows) * blockSize;
 }
